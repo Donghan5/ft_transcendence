cb889480e7fcd83cba6750ce346719237486e1fb
Donghan Kim <donghank@f4r12s10.clusters.42paris.fr>
2025-07-16 16:21:11 +0200

    change of the structures of project

---
+++ apps/backend/src/game/PongGameAIPlayer.ts
@@ -0,0 +1,2 @@
+// This file contains ai player logic for the Pong game.
+// It handles the AI's actions based on the game state and player positions.
---
+++ apps/backend/src/game/PongGameRule.ts
@@ -0,0 +1,1 @@
+// This file is define of the Pong game rule.
