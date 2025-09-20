9b862e5abce2a725100f8301a08c69b74c724cad
donghank <donghank@student.42.fr>
2025-09-20 23:51:52 +0200

    adding comment for ai

--- apps/backend/src/core/game/ai-strategy.ts
+++ apps/backend/src/core/game/ai-strategy.ts
@@ -38,53 +38,55 @@ class AiStrategy {
 	}
 
 	/**
-	 * to update the game state with AI decisions
+	 * @description to update the game state with AI decisions
+	 * @param game : current game state
 	 */
 	public updateAIPaddle(game: GameState): void {
-	const levelKey = (game.aiLevel?.toUpperCase() || 'MIDDLE') as keyof typeof AI_LEVEL;
-	const level = AI_LEVEL[levelKey] || AI_LEVEL.MIDDLE;   // if logic implemented, this level will be set by user input
-	const humanPlayer = game.player1;
-	const aiPlayer = game.player2;
-	const ball = game.ball;
+		const levelKey = (game.aiLevel?.toUpperCase() || 'MIDDLE') as keyof typeof AI_LEVEL;
+		const level = AI_LEVEL[levelKey] || AI_LEVEL.MIDDLE;   // if logic implemented, this level will be set by user input
+		const humanPlayer = game.player1;
+		const aiPlayer = game.player2;
+		const ball = game.ball;
 
-	// Drop out intended
-	if (Math.random() < level.mistakeChance) {
-        return;
-    }
-
-	const scoreDiff = aiPlayer.score - humanPlayer.score;
-	let strategy;
-
-	if (scoreDiff >= -2) {
-		strategy = this.AI_STRATEGIES.DEFENSIVE;
-	} else if (scoreDiff >= 2) {
-		strategy = this.AI_STRATEGIES.AGGRESSIVE;
-	} else {
-		strategy = this.AI_STRATEGIES.BALANCED;
-	}
-
-	if (ball.velocity.x > 0) {
-		let targetZ = predictBallPosition(ball, aiPlayer.position.x, level);
-
-		const diff = targetZ - aiPlayer.paddleZ;
-		const move = diff * level.speed;
-
-		aiPlayer.paddleZ += move;
-	} else {
-		const diff = 0 - aiPlayer.paddleZ;
-
-		aiPlayer.paddleZ += diff * 0.08;
-
-		if (aiPlayer.justHitBall) {
-			aiPlayer.justHitBall = false; // Reset the flag after moving back to center
+		// Drop out intended
+		if (Math.random() < level.mistakeChance) {
+			return;
 		}
-	}
 
-	const paddleLimit = 12;
-	if (aiPlayer.paddleZ > paddleLimit)
-		aiPlayer.paddleZ = paddleLimit;
-	if (aiPlayer.paddleZ < -paddleLimit)
-		aiPlayer.paddleZ = -paddleLimit;
+		const scoreDiff = aiPlayer.score - humanPlayer.score;
+		let strategy;
+
+		// Simple strategy selection based on score difference (I'm not sure that it works well)
+		if (scoreDiff >= -2) {
+			strategy = this.AI_STRATEGIES.DEFENSIVE;
+		} else if (scoreDiff >= 2) {
+			strategy = this.AI_STRATEGIES.AGGRESSIVE;
+		} else {
+			strategy = this.AI_STRATEGIES.BALANCED;
+		}
+
+		if (ball.velocity.x > 0) {
+			let targetZ = predictBallPosition(ball, aiPlayer.position.x, level);
+
+			const diff = targetZ - aiPlayer.paddleZ;
+			const move = diff * level.speed;
+
+			aiPlayer.paddleZ += move;
+		} else {
+			const diff = 0 - aiPlayer.paddleZ;
+
+			aiPlayer.paddleZ += diff * 0.08;
+
+			if (aiPlayer.justHitBall) {
+				aiPlayer.justHitBall = false; // Reset the flag after moving back to center
+			}
+		}
+
+		const paddleLimit = 13;  // Paddle movement boundaries (maybe 13...? / if not, change it to 12)
+		if (aiPlayer.paddleZ > paddleLimit)
+			aiPlayer.paddleZ = paddleLimit;
+		if (aiPlayer.paddleZ < -paddleLimit)
+			aiPlayer.paddleZ = -paddleLimit;
 	}
 }
 
