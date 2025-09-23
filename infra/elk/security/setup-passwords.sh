e6365b2d67cce4665df0f10905a116ef64ddea35
donghank <donghank@student.42.fr>
2025-09-23 20:19:21 +0200

    adding some sleep

--- infra/elk/security/setup-passwords.sh
+++ infra/elk/security/setup-passwords.sh
@@ -14,13 +14,13 @@ echo "Setting kibana_system password..."
 curl -s -X POST -u ${ELASTIC_USER}:${ELASTIC_PASSWORD} -H "Content-Type: application/json" \
   http://elasticsearch:9200/_security/user/${KIBANA_USER}/_password \
   -d "{\"password\":\"${KIBANA_PASSWORD}\"}"
-sleep 2
+sleep 5
 
 echo "Creating logstash_writer role..."
 curl -s -X PUT -u ${ELASTIC_USER}:${ELASTIC_PASSWORD} -H "Content-Type: application/json" \
   http://elasticsearch:9200/_security/role/logstash_writer \
   -d '{"cluster":["manage_index_templates","monitor","manage_ilm"],"indices":[{"names":["pong-logs-*","logstash-*"],"privileges":["write","create","create_index","manage","manage_ilm"]}]}'
-sleep 2
+sleep 5
 
 echo "Creating logstash_internal user..."
 curl -s -X POST -u ${ELASTIC_USER}:${ELASTIC_PASSWORD} -H "Content-Type: application/json" \
